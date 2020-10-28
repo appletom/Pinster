dcf7df26fbd4f65a10c77f654503a41c0ce41058
allawa1 <alexiaallaway@gmail.com>
2020-10-28 18:19:48 -0400

    html runs on PORT:3000

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express()
 //simple server running on PORT 3000
 const port = 3000
 
+app.use("/css", express.static(__dirname + "/public/css"))
+app.use("/", express.static(__dirname + "/public/html"))
+
+
 app.get('/', function (req, res) {
     res.send('PONG')
 })
