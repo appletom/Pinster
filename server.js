07ca3e4f58917ce43a21576f7119e7b87ef48b55
allawa1 <alexiaallaway@gmail.com>
2020-10-29 20:52:52 -0400

    ignore

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
+const tumblrApi = require('./server/tumblrApi/tmblrAuth')
 //simple server running on PORT 3000
 const port = 3000
 
+
+app.use("/app", express.static(__dirname + "/public/app"))
 app.use("/css", express.static(__dirname + "/public/css"))
 app.use("/", express.static(__dirname + "/public/html"))
+
+
+
 
 
 app.get('/', function (req, res) {
     res.send('PONG')
 })
+
 
 
 app.listen(port, ()=>{
