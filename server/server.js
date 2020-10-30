f5967617331f81c39f1ac1d036396f5fc0868fc2
Christy <appletoms99@outlook.com>
2020-10-29 19:52:44 -0500

    update server.js and add apiRoutes folder+file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,38 @@
+require("dotenv").config();
+
+// require express
 const express = require('express')
 const app = express()
+
 //simple server running on PORT 3000
 const port = 3000
+
+//require body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
 
 app.get('/', function (req, res) {
     res.send('PONG')
 })
+
+
+
+//connect server to public folder and files
+app.use("/", express.static(__dirname + "/public"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
+
+
+//connect server to api routers
+const apiRouters = require("./apiRoutes/routers");
+app.use("/apiRoutes/routers", apiRouters)
+
+
+
+
 
 
 app.listen(port, ()=>{
