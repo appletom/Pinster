d3feea7b15ed730d9829cdc1346e951f62c1aafd
Daisy Umotong <x4daisy@rocketmail.com>
2020-11-04 23:15:55 -0800

    added ejs navs

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
+// Imports
 require("dotenv").config();
 
-
-
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const tumblrApi = require('./apiRoutes/tumblr');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-
-
-//simple server running on PORT 3000
-const port = 3000
+const port = 3000;
 const db = require('./PinsterDB/models')
 
+// Static files setup
 app.use(bodyParser.json());
-
-app.use("/app", express.static(__dirname + "/public/app"));
+app.use(express.static('public'));
+//app.use("/app", express.static(__dirname + "/public/app"));
 app.use("/css", express.static(__dirname + "/public/css"));
-app.use("/", express.static(__dirname + "/public/html"));
+//app.use("/", express.static(__dirname + "/public/html"));
+app.use("/img", express.static(__dirname + "public/img"));
+
+// Set templating engine
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//EJS Layout Navigation
+app.get('', (req, res) => {
+    res.render('index')
+});
 
 //SEQUELIZE TEST
 // db.sequelize.authenticate().then( ()=> {
@@ -25,7 +33,7 @@ app.use("/", express.static(__dirname + "/public/html"));
 // }).catch( ()=>{
 //     console.log("There was an errro")
 // })
-db.sequelize.sync()
+//db.sequelize.sync()
 
 
 app.get('/', function (req, res) {
@@ -34,16 +42,11 @@ app.get('/', function (req, res) {
 
 tumblrApi(app, fetch);
 
-
-
-
-//connect server to api routers
+//Connect server to api routers
 const apiRouters = require("./apiRoutes/routers");
 app.use("/apiRoutes/routers", apiRouters)
 
-
-
-
+// Listen on Port X
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
-})
+});
